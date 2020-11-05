a2af3529ed36b7be6df1a6eaf9e096e075e9baa2
jhchen-NCNU <jhchen@ncnu.edu.tw>
2020-11-05 14:36:34 +0800

    1.1B

--- todoRpt.php
+++ todoRpt.php
@@ -4,6 +4,7 @@ require("dbconnect.php");
 
 $sql = "select * from todo where status = 1;";
 $result=mysqli_query($conn,$sql) or die("DB Error: Cannot retrieve message.");
+//comment from B
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
